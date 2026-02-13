291646b76f7f8dcdb47a8fa9c71c62db023257e7
Mirko Mariotti <mirko@mirkomariotti.it>
2026-02-13 21:18:10 +0100

    :sparkles: (rulemancer) dashboard and game web interfaces

--- pkg/rulemancer/webclient.go
+++ pkg/rulemancer/webclient.go
@@ -1,40 +1,15 @@
 package rulemancer
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 
 	chi "github.com/go-chi/chi/v5"
 )
-
-func (e *Engine) templateGenerator(baseDir, templateDir string) (map[string]string, error) {
-	templateMap := make(map[string]string)
-
-	if templateFiles, err := os.ReadDir(baseDir + "/" + templateDir); err == nil {
-		// Process each template file
-		for _, file := range templateFiles {
-			if !file.IsDir() {
-				content, err := os.ReadFile(baseDir + "/" + templateDir + "/" + file.Name())
-				if err != nil {
-					return nil, err
-				}
-				templateMap[templateDir+"/"+file.Name()] = string(content)
-			} else {
-				subTemplateMap, err := e.templateGenerator(baseDir, templateDir+"/"+file.Name())
-				if err != nil {
-					return nil, err
-				}
-				for k, v := range subTemplateMap {
-					templateMap[k] = v
-				}
-			}
-		}
-	}
-
-	return templateMap, nil
-}
 
 func (e *Engine) handlerGenerator(path string, templateMap map[string]string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -44,19 +19,98 @@ func (e *Engine) handlerGenerator(path string, templateMap map[string]string) fu
 
 func (e *Engine) webClientRoutes(r chi.Router) {
 
-	templateDir := "pkg/rulemancer/templates/webclient"
-
-	templateMap, err := e.templateGenerator(templateDir, "")
+	webPages, err := e.webPagesReady()
 	if err != nil {
 		if e.Debug {
-			l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/SpawnWebClient]")+" ", 0)
-			l.Printf("Failed to generate templates: %v", err)
+			l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/webClientRoutes]")+" ", 0)
+			l.Printf("Failed to prepare web pages: %v", err)
 		}
+		return
 	}
-	fmt.Println(templateMap)
+	fmt.Printf("Web pages ready: %v\n", webPages)
 	r.Route("/", func(r chi.Router) {
-		for path := range templateMap {
-			r.Get(path, e.handlerGenerator(path, templateMap))
+		for path := range webPages {
+			r.Get(path, e.handlerGenerator(path, webPages))
 		}
 	})
+}
+
+func (e *Engine) webPagesReady() (map[string]string, error) {
+	templateDir := "pkg/rulemancer/templates/webclient"
+
+	templateMap, err := e.webTemplates(templateDir, "")
+	if err != nil {
+		if e.Debug {
+			l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/webPagesReady]")+" ", 0)
+			l.Printf("Failed to generate templates: %v", err)
+		}
+		return nil, fmt.Errorf("failed to generate templates: %w", err)
+	}
+
+	// Load each game relations information about slots and multislots
+	gamesInterfaces, err := e.gameInterfaces()
+	if err != nil {
+		if e.Debug {
+			l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/webPagesReady]")+" ", 0)
+			l.Printf("Error loading game interfaces: %v", err)
+		}
+		return nil, fmt.Errorf("failed to load game interfaces: %w", err)
+	}
+
+	webPages := make(map[string]string)
+
+	for gameName, pd := range gamesInterfaces {
+		if e.Debug {
+			l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, yellow("[rulemancer/webPagesReady]")+" ", 0)
+			l.Printf("Generating shell files for game: %s", gameName)
+		}
+
+		// Execute each template
+		for templateName, templateContent := range templateMap {
+			tmpl, err := template.New(templateName).Funcs(pd.funcMap).Parse(templateContent)
+			if err != nil {
+				if e.Debug {
+					l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/webPagesReady]")+" ", 0)
+					l.Printf("Error parsing template %s for game %s: %v", templateName, gameName, err)
+				}
+				return nil, fmt.Errorf("failed to parse template %s for game %s: %w", templateName, gameName, err)
+			}
+
+			switch templateName {
+			case "/client/game":
+				var pageContent bytes.Buffer
+
+				if err := tmpl.Execute(&pageContent, pd); err != nil {
+					if e.Debug {
+						l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/webPagesReady]")+" ", 0)
+						l.Printf("Error executing template %s for game %s: %v", templateName, gameName, err)
+					}
+					return nil, fmt.Errorf("failed to execute template %s for game %s: %w", templateName, gameName, err)
+				}
+				if e.Debug {
+					l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, yellow("[rulemancer/webPagesReady]")+" ", 0)
+					l.Printf("Generated web page for template %s of game %s", templateName, gameName)
+				}
+				webPages["/client/"+gameName] = pageContent.String()
+			default:
+				if _, ok := webPages[templateName]; !ok {
+					var pageContent bytes.Buffer
+
+					if err := tmpl.Execute(&pageContent, pd); err != nil {
+						if e.Debug {
+							l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, red("[rulemancer/webPagesReady]")+" ", 0)
+							l.Printf("Error executing template %s for game %s: %v", templateName, gameName, err)
+						}
+						return nil, fmt.Errorf("failed to execute template %s for game %s: %w", templateName, gameName, err)
+					}
+					if e.Debug {
+						l := log.New(&writer{os.Stdout, "2006-01-02 15:04:05 "}, yellow("[rulemancer/webPagesReady]")+" ", 0)
+						l.Printf("Generated web page for template %s of game %s", templateName, gameName)
+					}
+					webPages[templateName] = pageContent.String()
+				}
+			}
+		}
+	}
+	return webPages, nil
 }
